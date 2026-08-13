436de48f714df50caf9815673fcfc2c879a3d6b4
Factorkevin <factorkevin24@gmail.com>
2026-08-13 14:30:17 +1000

    Daily Practices

---
+++ practice.rb
@@ -0,0 +1,8 @@
+# Write a program that uses a variable to store a number, then prints that number plus 15.
+
+year = 2011
+puts year + 15
+
+# Write a program that uses a variable to store a word, then prints that word with all capital letters.
+name = "kevin"
+puts name.upcase 
