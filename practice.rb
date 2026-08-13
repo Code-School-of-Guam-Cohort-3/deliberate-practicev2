6880ae94d326c98dad3660e92b36b28cc31aeb51
Factorkevin <factorkevin24@gmail.com>
2026-08-13 14:44:40 +1000

    Add practice problem

--- practice.rb
+++ practice.rb
@@ -6,3 +6,10 @@ puts year + 15
 # Write a program that uses a variable to store a word, then prints that word with all capital letters.
 name = "kevin"
 puts name.upcase 
+
+# Write a program that uses variables to store two numbers, then prints the numbers multiplied together.
+price = 12.50
+quantity = 4
+
+total = price * quantity
+
