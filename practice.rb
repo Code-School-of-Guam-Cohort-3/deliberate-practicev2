926db9ecfda23833c34ca6e3e8aba9ae35687f94
Factorkevin <factorkevin24@gmail.com>
2026-08-20 14:01:59 +1000

    Add exercise solutions

--- practice.rb
+++ practice.rb
@@ -12,4 +12,44 @@ price = 12.50
 quantity = 4
 
 total = price * quantity
+puts total 
 
+# Write a program that uses a variable to store a word, then prints that word in reverse order.
+car = "racecar"
+puts car.reverse
+
+# Write a program that uses a variable to store a number, then prints the number divided by 5.
+pizza = 20
+puts pizza / 5
+
+# Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+first = "Kevin"
+last = "Factor"
+puts first.upcase + " " + last.upcase
+# Write a program that uses a variable to store a word, then prints the number of letters in the word.
+river = "mississippi"
+puts river.length
+
+# Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+oven_temp = 350.95
+puts oven_temp.to_i
+
+# Write a program that uses a variable to store a number, then prints the number squared.
+area_length = 6
+puts area_length ** 2
+# Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+
+country = "JAPAN"
+puts country.downcase
+
+# CONDITIONALS
+
+# Use a variable to store a number, then write a condition that prints 
+# "Perfect" if the number is equal to 25, and prints "Not quite" otherwise.
+points = 26
+if points == 25
+  puts "Perfect"
+else
+  puts "Not quite"
+end
